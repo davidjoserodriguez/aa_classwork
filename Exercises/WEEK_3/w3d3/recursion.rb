559858e85401d585d01e24302fa8128f76cc0483
David Rodriguez <d.joseph.rodriguez@gmail.com>
2021-07-21 11:49:57 -0700

    exp1,exp2,deep_dup

--- Exercises/WEEK_3/w3d3/recursion.rb
+++ Exercises/WEEK_3/w3d3/recursion.rb
@@ -23,6 +23,44 @@ def range(min, max)
     # ^ num       ^ same data type our base case
 end
 
-
-
 # p range(5, 10)
+
+def exp1(base, power)
+    return 1 if power == 0
+    base * exp1(base, power - 1)
+end
+
+def exp2(base, power)
+    return 1 if power == 0
+    return base if power == 1
+
+    if power.even?
+        base * exp2(base, power / 2) ** 2
+    else
+        base * (exp2(base, (power - 1) / 2) ** 2)
+    end
+end
+
+def deep_dup(arr)
+    return [arr] unless arr.is_a? # == if !array.is_a?
+    deep_copy = []
+
+    arr.each do |ele|
+        deep_copy << deep_dup(ele)
+    end
+    
+    deep_copy
+end
+
+
+robot_parts = [
+  ["nuts", "bolts", "washers"],
+  ["capacitors", "resistors", "inductors"]
+]
+
+robot_parts_copy = robot_parts.dup
+
+# shouldn't modify robot_parts
+robot_parts_copy[1] << "LEDs"
+# but it does
+print robot_parts[1] # => ["capacitors", "resistors", "inductors", "LEDs"]
