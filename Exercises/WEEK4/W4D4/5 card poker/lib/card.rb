74dab6dc28e13bc9ed48a37a016f13ecd59d7372
David Rodriguez <d.joseph.rodriguez@gmail.com>
2021-07-29 16:25:51 -0700

    Added Card

--- Exercises/WEEK4/W4D4/5 card poker/lib/card.rb
+++ Exercises/WEEK4/W4D4/5 card poker/lib/card.rb
@@ -1,5 +1,30 @@
 class Card
-    
+
+    SUIT = ["D", "S", "C", "H"]
+    CARDS = {
+        "A"=> 14,
+        "K"=>13,
+        "Q"=>12,
+        "J"=>11,
+        "10"=> 10,
+        "9"=> 9,
+        "8"=> 8,
+        "7"=> 7,
+        "6"=> 6,
+        "5"=> 5,
+        "4"=> 4,
+        "3"=> 3,
+        "2"=> 2
+    }
+
+
+    attr_reader :num, :suit
+    def initialize(num, suit)
+        raise "That is not a valid type of card" if !SUIT.include?(suit)
+        raise "That is not a valid num" if !CARDS.has_key?(num)
+        @num = num
+        @suit = suit
+    end
 
 
 end
--- Exercises/WEEK4/W4D4/5 card poker/spec/card_spec.rb
+++ Exercises/WEEK4/W4D4/5 card poker/spec/card_spec.rb
@@ -2,8 +2,25 @@ require "card"
 
 describe "Card" do
     describe "#initialize" do
-        
+        it "set up instance variables" do
+            num = "5"
+            suit = "D"
+            card = Card.new(num, suit)
+            expect(card.num).to eq(num)
+            expect(card.suit).to eq(suit)
+        end
 
+        it "should raise error if suit is invalid" do
+            num = "5"
+            suit = "A"
+            expect {Card.new(num, suit)}.to raise_error("That is not a valid type of card")
+        end
+
+        it "should raise error if num is invalid" do
+            num = "1000000"
+            suit = "D"
+            expect {Card.new(num, suit)}.to raise_error("That is not a valid num")
+        end
     end
 
 end
