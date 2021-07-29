f365ae402e72cd9ec2b6669812f16dd9837c2f20
Tony Zhang <tozhang665@gmail.com>
2021-07-29 12:07:41 -0700

    Added Stock_picker and transpose

--- Exercises/WEEK4/W4D4/lib/tdd.rb
+++ Exercises/WEEK4/W4D4/lib/tdd.rb
@@ -8,7 +8,6 @@ class Array
         end
         arr 
     end
-
     def two_sum
         arr = []
         self.each_with_index do |el, idx|
@@ -17,7 +16,43 @@ class Array
             end
         end
         arr
-    end
-    
-    
+    end 
 end
+
+# [[0,3,6],
+# [3,4,7],
+# [6,7,8]]
+
+# (0...2).each do i
+#  (0..3)
+
+
+def my_transpose(arr)
+    arr.transpose
+end
+
+#[5,2,7,6,10]
+# 5 2 - 2 is smaller so skip
+# 5 7 - 7-5 = 2 gain    < -- current biggest [0,2]
+# 5,6 = 6- 5 = 1
+#5 , 10 = 5 gain <---new biggest
+#2,7
+
+def stock_picker(arr)
+    biggest = 0
+    biggestArray = []
+    (0...arr.length - 1).each do |idx1|
+        (idx1+1 ... arr.length).each do |idx2|
+            current = [idx1,idx2]
+            profit = arr[idx2] - arr[idx1]
+            if profit > 0 && profit > biggest
+                biggest = profit
+                biggestArray = current
+            end
+        end
+    end
+    biggestArray
+end
+
+
+#
--- Exercises/WEEK4/W4D4/spec/tdd_spec.rb
+++ Exercises/WEEK4/W4D4/spec/tdd_spec.rb
@@ -11,3 +11,15 @@ describe "#two_sum" do
         expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
     end
 end
+
+describe "#my_transpose" do 
+    it "will convert between the row-oriented and column-oriented respresentations" do
+        expect(my_transpose([[0,1,2],[3,4,5],[6,7,8]])).to eq([[0,3,6],[1,4,7],[2,5,8]])
+    end
+end
+
+describe "#stock_picker" do
+    it "returns an array of stock prices and the most profitable pair" do 
+        expect(stock_picker([5,2,7,6,10])).to eq([1,4])
+    end
+end
