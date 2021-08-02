d0c5b89d719d0070ace2012ff06c7878ca813b0e
IanDMcGrath <ian92415@gmail.com>
2021-08-02 16:31:55 -0700

    added hashing

--- Exercises/WEEK5/w5d1/HashMap_LRUCache/lib/p02_hashing.rb
+++ Exercises/WEEK5/w5d1/HashMap_LRUCache/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    total = 0
+    self.each.with_index do |el, i|
+      total += el.object_id * (i + 1)
+    end
+    total.hash
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a + ("A".."Z").to_a
+    total = 0
+    self.each_char.with_index do |char, i|
+      total += alpha.index(char) * (i + 1)
+    end
+    total.hash
   end
 end
 
@@ -16,6 +27,16 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    alpha_bet = ("a".."z").to_a + ("A".."Z").to_a
+    alpha_symbol = (:a..:z).to_a + (:A..:Z).to_a
+    total = 0
+    i = 0
+    self.each do |k,v|
+      i += 1
+      # p "k:#{k} // v:#{v} // i:#{i}"
+      total += (alpha_symbol.index(k) + (alpha_bet.index(v))) # nil error somewhere?
+    end
+    return total.hash
+
   end
 end
