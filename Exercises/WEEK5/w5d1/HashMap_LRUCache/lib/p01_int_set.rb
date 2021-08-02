09da22dfc0fdfc2e5d85d63275267290700e4c99
David Rodriguez <d.joseph.rodriguez@gmail.com>
2021-08-02 15:37:12 -0700

    Add int

--- Exercises/WEEK5/w5d1/HashMap_LRUCache/lib/p01_int_set.rb
+++ Exercises/WEEK5/w5d1/HashMap_LRUCache/lib/p01_int_set.rb
@@ -39,18 +39,25 @@ class IntSet
   end
 
   def insert(num)
+
+    if !include?(num)
+      self[num] << num
+    end
+    
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -67,18 +74,33 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !include?(num)
+      self[num] << num
+      @count += 1
+    end
+
+    if !(num_buckets > @count)
+      resize!
+    end
+
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+    
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -86,5 +108,20 @@ class ResizingIntSet
   end
 
   def resize!
+    dup = @store.map { |bucket| bucket.dup }
+
+    @store = Array.new(num_buckets * 2){Array.new}
+
+    
+
+    dup.each do |bucket|
+      bucket.each do |obj|
+        self[obj] << obj
+      end
+    end
+        
+        # new_obj = obj
+        # @store[i].delete(obj)
+        # self.insert(obj)
   end
 end
